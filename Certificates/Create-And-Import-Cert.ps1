e7850aa35a36ac1e22020743b156c633b1031b0b
nli <nli@magnetix.dk>
2020-07-01 10:38:06 +0200

    update certificate script to be neos specific

--- Certificates/Create-And-Import-Cert.ps1
+++ Certificates/Create-And-Import-Cert.ps1
@@ -2,7 +2,7 @@
 # Creates a certificate that is automatically imported into the "Trusted Root Certification Authorities"
 # The certificate should be imported manually into the Personal store by opening certmgr afterwards
 
-$thumbprint = (New-SelfSignedCertificate -DnsName "BunningsTest" -Subject "BunningsTest" -CertStoreLocation cert:\LocalMachine\My -NotAfter (Get-Date).AddMonths(120) -FriendlyName "SelfSigned Cert for BunningsTest").Thumbprint 
+$thumbprint = (New-SelfSignedCertificate -DnsName "*.sdcneos.local" -Subject "*.sdcneos.local" -CertStoreLocation cert:\LocalMachine\My -NotAfter (Get-Date).AddMonths(120) -FriendlyName "*.sdcneos.local").Thumbprint 
 $pwd = ConvertTo-SecureString -String "password" -Force -AsPlainText
-Export-PfxCertificate -Password $pwd -Cert cert:\localmachine\My\$thumbprint -FilePath "E:\Temp\BunningTest.pfx"
-Import-PfxCertificate -Password $pwd -Cert cert:\localmachine\Root -FilePath "E:\Temp\BunningTest.pfx"
+Export-PfxCertificate -Password $pwd -Cert cert:\localmachine\My\$thumbprint -FilePath "C:\Users\nicholas.lindboe\OneDrive - Dentsu Aegis Network\Documents\Misc\temp\neos.pfx"
+Import-PfxCertificate -Password $pwd -Cert cert:\localmachine\Root -FilePath "C:\Users\nicholas.lindboe\OneDrive - Dentsu Aegis Network\Documents\Misc\temp\neos.pfx"
